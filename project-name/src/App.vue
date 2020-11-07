7dee287d67d94c190a7029f9fb7d838a33b5881b
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2020-11-07 15:16:36 +0100

    Listado inicial lateral

---
+++ project-name/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <ListadoStarWars>
+    </ListadoStarWars>
+  </div>
+</template>
+
+<script>
+import ListadoStarWars from './components/ListadoStarWars.vue'
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+    }
+  },
+  components: {
+    ListadoStarWars
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ project-name/src/components/ListadoStarWars.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="ListadoStarWars">
+    <h2>Vehicles</h2>
+    <div>
+    </div>
+    <div class="lista-vehiculos">
+      <ul>
+        <li v-for="(vehicle, x) in list.results" v-bind:key="x">
+          {{ vehicle.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "ListadoStarWars",
+  data() {
+    return {
+      list: Array
+    }
+  },
+  mounted() {
+    fetch('https://swapi.dev/api/vehicles/')
+        .then(res => res.json())
+        .then(resultado => {
+          console.log(resultado);
+          this.list = resultado;
+        })
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+  }
+  #ListadoStarWars {
+    width: 20%;
+    text-align: left;
+  }
+</style>
